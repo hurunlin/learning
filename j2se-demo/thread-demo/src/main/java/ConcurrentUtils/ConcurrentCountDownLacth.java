68eca6cc9011296db6a82d973e02dce22f7f280a
runlinhu@163.com <runlinhu@163.com>
2017-05-29 15:44:27 +0800

    并发工具类学习！！ 1、concurrentCountDownLacth类学习demo

--- j2se-demo/thread-demo/src/main/java/ConcurrentUtils/ConcurrentCountDownLacth.java
+++ j2se-demo/thread-demo/src/main/java/ConcurrentUtils/ConcurrentCountDownLacth.java
@@ -1,8 +1,70 @@
+/**
+ * @Title: ConcurrentCountDownLacth
+ * @Package ConcurrentUtils
+ * @Description: 并发工具类
+ * @author hu.rl
+ * @date 2017/5/29 9:44
+ * @version V1.0
+ */
 package ConcurrentUtils;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
- * 并发工具类
- * 1、
+ * @author hu.rl
+ * @ClassName: ConcurrentCountDownLacth
+ * @Description: 并发工具类
+ * @date 2017/5/29 9:44
  */
 public class ConcurrentCountDownLacth {
+
+
+    /**
+     * @param
+     * @return
+     * @throws
+     * @Description: [主函数入口]
+     * @date 2017/5/29  15:14
+     */
+    public static void main(String[] args) {
+        /**
+         * 1: 创建在线程中使用的countDown创建在线程中使用的countDown
+         * 2: 参数(1)需要有多少个任务需要初始化完成,1则需要有一个线程调用countDown
+         */
+
+        final CountDownLatch countDown = new CountDownLatch(2);
+        // 创建线程1
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("线程T1 等待其他线程处理完成");
+                try {
+                    countDown.await();
+                    System.out.println("T1线程继续执行");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // 创建线程2
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("T2线程进行初始化操作");
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("T2线程业务处理完成");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // 通知T1线程继续执行
+                countDown.countDown();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
 }
