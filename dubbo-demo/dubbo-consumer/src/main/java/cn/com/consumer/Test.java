aa59bb2ecc98138f030db2fabdbd1a80b43d8050
runlinhu@163.com <runlinhu@163.com>
2017-06-18 10:19:05 +0800

    Merge branch 'master' of github.com:hurunlin/learning
    # Conflicts:
    #	spring-demo/logs/mybatis/consol.log

---
+++ dubbo-demo/dubbo-consumer/src/main/java/cn/com/consumer/Test.java
@@ -0,0 +1,18 @@
+/**
+ * @Title: Test
+ * @Package cn.com.consumer
+ * @Description: 测试类
+ * @author hu.rl
+ * @date 2017/6/18 10:18
+ * @version V1.0
+ */
+package cn.com.consumer;
+
+/**
+ * @ClassName: Test
+ * @Description: 测试类
+ * @author hu.rl
+ * @date 2017/6/18 10:18
+ */
+public class Test {
+}
