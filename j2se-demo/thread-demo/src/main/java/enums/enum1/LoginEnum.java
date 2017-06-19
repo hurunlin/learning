53517b2a212b6b55bece0946d8b66ace69f942c8
hurl <hu.rl@sand.com.cn>
2017-06-19 17:38:55 +0800

    使用枚举类型参数

---
+++ j2se-demo/thread-demo/src/main/java/enums/enum1/LoginEnum.java
@@ -0,0 +1,33 @@
+/**
+ * Project Name: learning
+ * File Name: LoginEnum.java
+ * Package Name: enums.enum1
+ * Date: 2017-06-19 17:32
+ * Copyright (c) 2016, 杉德巍康企业服务有限公司.
+ *
+ * @author hu.rl
+ */
+package enums.enum1;
+
+/**
+ * ClassName: LoginEnum <br>
+ * Function: <br>
+ * Date:  2017-06-19 17:32 <br>
+ */
+public enum LoginEnum {
+    QQLOGIN("qqLogin"), SINALOGIN("sinaLogin");
+
+    private String type;
+
+    private LoginEnum(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ j2se-demo/thread-demo/src/main/java/enums/enum1/Test.java
@@ -0,0 +1,31 @@
+/**
+ * Project Name: learning
+ * File Name: Test.java
+ * Package Name: enums.enum1
+ * Date: 2017-06-19 17:32
+ * Copyright (c) 2016, 杉德巍康企业服务有限公司.
+ *
+ * @author hu.rl
+ */
+package enums.enum1;
+
+/**
+ * ClassName: Test <br>
+ * Function: <br>
+ * Date:  2017-06-19 17:32 <br>
+ */
+public class Test {
+
+    /**
+     * 使用枚举类型的参数
+     * @param loginEnum
+     */
+    public static void test(LoginEnum loginEnum) {
+        System.out.println(loginEnum.getType());
+    }
+
+
+    public static void main(String[] args) {
+        Test.test(LoginEnum.QQLOGIN);
+    }
+}
