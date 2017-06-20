c5dc75a4e0ed0b2867e7b5122527ec96b06424a1
hurl <hu.rl@sand.com.cn>
2017-06-20 16:02:16 +0800

    项目部分优化

--- mware-demo/es-demo/src/main/java/Test.java
+++ mware-demo/es-demo/src/main/java/Test.java
@@ -1,18 +1,17 @@
 /**
  * Project Name: learning
  * File Name: Test.java
- * Package Name: cn.com.es
- * Date: 2017-06-20 14:56
+ * Package Name: PACKAGE_NAME
+ * Date: 2017-06-20 16:02
  * Copyright (c) 2016, 杉德巍康企业服务有限公司.
  *
  * @author hu.rl
  */
-package cn.com.es;
 
 /**
  * ClassName: Test <br>
  * Function: <br>
- * Date:  2017-06-20 14:56 <br>
+ * Date:  2017-06-20 16:02 <br>
  */
 public class Test {
 }
