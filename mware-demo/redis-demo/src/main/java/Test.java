6f626bee5bc5d1eec3cc9c27d3d8b89aaf0123e6
hurl <hu.rl@sand.com.cn>
2017-06-20 15:58:40 +0800

    项目部分优化

---
+++ mware-demo/redis-demo/src/main/java/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Project Name: learning
+ * File Name: Test.java
+ * Package Name: PACKAGE_NAME
+ * Date: 2017-06-20 15:58
+ * Copyright (c) 2016, 杉德巍康企业服务有限公司.
+ *
+ * @author hu.rl
+ */
+
+/**
+ * ClassName: Test <br>
+ * Function: <br>
+ * Date:  2017-06-20 15:58 <br>
+ */
+public class Test {
+}
