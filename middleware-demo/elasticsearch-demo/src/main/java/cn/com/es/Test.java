8e32ce6a1a97938281e092ba9d100c3ddc936d8d
hurl <hu.rl@sand.com.cn>
2017-06-20 14:56:43 +0800

    使用枚举类型参数

---
+++ middleware-demo/elasticsearch-demo/src/main/java/cn/com/es/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Project Name: learning
+ * File Name: Test.java
+ * Package Name: cn.com.es
+ * Date: 2017-06-20 14:56
+ * Copyright (c) 2016, 杉德巍康企业服务有限公司.
+ *
+ * @author hu.rl
+ */
+package cn.com.es;
+
+/**
+ * ClassName: Test <br>
+ * Function: <br>
+ * Date:  2017-06-20 14:56 <br>
+ */
+public class Test {
+}
